b16d17c0b87351a268c21ff6b969512878b4a62c
Miles Heise <milesheise@gmail.com>
2017-12-05 14:13:23 -0500

    initialize

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,22 @@
-Class Kele
+require 'httparty'
+
+class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, pass)
-    @auth_token = self.class.post(email: email, password: pass).auth_token
+    response = self.class.post(
+      '/sessions', body: {
+        email: email,
+        password: pass
+      }
+    )
+    if response['message'] == 'Resource not found'
+      p 'oh god no'
+    else
+      p response['auth_token']
+      @auth_token = response['auth_token']
+    end
+    p @auth_token
   end
 end
