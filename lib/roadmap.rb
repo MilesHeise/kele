d356f276934ec5ec4a3cb7a766a4eca94f784ccf
Miles Heise <milesheise@gmail.com>
2017-12-05 19:10:53 -0500

    roadmap module added

---
+++ lib/roadmap.rb
@@ -0,0 +1,23 @@
+module Roadmap
+  def get_roadmap(id)
+    response = self.class.get(
+      "/roadmaps/#{id}", headers: {
+        'authorization' => @auth_token
+      }, body: {
+        id: id
+      }
+    )
+    JSON.parse(response.body)
+  end
+
+  def get_checkpoint(id)
+    response = self.class.get(
+      "/checkpoints/#{id}", headers: {
+        'authorization' => @auth_token
+      }, body: {
+        id: id
+      }
+    )
+    JSON.parse(response.body)
+  end
+end
