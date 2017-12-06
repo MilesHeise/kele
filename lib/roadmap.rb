79ad526096c917b9d48cc6494bf2e006ef4e44c0
Miles Heise <milesheise@gmail.com>
2017-12-06 14:51:11 -0500

    partial fulfillment checkpoints

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -20,4 +20,23 @@ module Roadmap
     )
     JSON.parse(response.body)
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(
+      '/messages', headers: {
+        'authorization' => @auth_token
+      }, body: {
+        'enrollment_id' => get_me.dig('current_enrollment', 'id'),
+        'checkpoint_id' => checkpoint_id,
+        'assignment_branch' => however I do optional,
+        'assignment_commit_link' => however I do optional,
+        'comment' => however I do optional
+      }
+    )
+    if response['message']
+      p 'message not sent'
+    else
+      p 'message sent successfully'
+    end
+  end
 end
